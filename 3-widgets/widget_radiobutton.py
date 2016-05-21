817b763802bf3961f282642be62fb423989ac8db
paulm29 <paul.miscellaneous@gmail.com>
2016-05-22 08:57:02 +1000

    Workaround for Windows: deselect all radio buttons by default

--- 3-widgets/widget_radiobutton.py
+++ 3-widgets/widget_radiobutton.py
@@ -6,6 +6,7 @@ r1 = Radiobutton(root, text = "SPAM", variable = breakfast, value = 'SPAM')
 r2 = Radiobutton(root, text = "Eggs", variable = breakfast, value = 'Eggs')
 r3 = Radiobutton(root, text = "Sausage", variable = breakfast, value = 'Sausage')
 r4 = Radiobutton(root, text = "Iggy", variable = breakfast, value = 'Iggy')
+breakfast.set(None) # have to set so that not all are selected on Windows
 
 r1.grid(row=0,column=0)
 r2.grid(row=1,column=0)
