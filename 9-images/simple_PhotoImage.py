90f0b0db1c896fe2ea1013923d129f42584ecf31
paulm29 <paul.miscellaneous@gmail.com>
2016-05-27 17:42:46 +1000

    Combine PhotoImage examples for Labels and Buttons

--- 9-images/simple_PhotoImage.py
+++ 9-images/simple_PhotoImage.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 root = Tk()
 
+# Labels
 photo = PhotoImage(file="save.png")
 label = Label(root, image=photo)
 label.grid(row=0,column=0)
@@ -11,6 +12,18 @@ label.grid(row=0,column=0)
 photo = PhotoImage(file="python_logo.png")
 label = Label(root, image=photo)
 label.pack()
+'''
+
+# Buttons
+photo2 = PhotoImage(file="save.png")
+button = Button(root, image=photo2)
+button.grid(row=1,column=0)
+
+'''
+# pack version
+photo = PhotoImage(file="save.png")
+button = Button(root, image=photo)
+button.pack()
 '''
 
 root.mainloop()
