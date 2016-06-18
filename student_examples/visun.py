c32e6ad0a30e977e13991bbe0009968d643afae0
paulm29 <paul.miscellaneous@gmail.com>
2016-06-18 17:38:30 +1000

    Update visun example

--- student_examples/visun.py
+++ student_examples/visun.py
@@ -1,39 +1,30 @@
-from tkinter import * #import all the tkinter classes so that we can use them
+from tkinter import *
 
-root = Tk()  # create a blank window called root
+root = Tk()
 
-root.title("Booking System for Brisbane Airport")
-root.geometry("450x300+100+100") # width x height + x position + y position
-#root.wm_iconbitmap('people.ico')
-root.configure(background="#e6e6fa") #lavender
+#IMAGE CODE
+photo = PhotoImage(file="harold2.png")
+label = Label(root, image=photo)
+label.grid(row=11,column=15)
+label.grid_remove() # hide the image
 
-def bookflight():
-    print("I'm going to 9/11 this plane.")
+def hideThePainHarold():
+    label.grid() # show the image
 
-myButton = Button(root, text="Book this flight", command=bookflight)
-myButton.grid(row=0,column=4)
-myButton = Button(root, text="Book this flight", command=bookflight)
-myButton.grid(row=1, column=4)
-myButton = Button(root, text="Book this flight", command=bookflight)
-myButton.grid(row=1, column=4)
-myButton = Button(root, text="Book this flight", command=bookflight)
-myButton.grid(row=3, column=4)
-myButton = Button(root, text="Book this flight", command=bookflight)
-myButton.grid(row=4, column=4)
-myButton = Button(root, text="Book this flight", command=bookflight)
-myButton.grid(row=5, column=4)
-myButton = Button(root, text="Book this flight", command=bookflight)
-myButton.grid(row=6, column=4)
 
-myListbox = Listbox(root,exportselection=0, width=40)
-myListbox.insert(2, "Brisbane to JFK (Departure 6:45pm)")
-myListbox.insert(3, "Brisbane to Wellington (Departure 6:45pm)")
-myListbox.insert(4, "Brisbane to Hong Kong (Departure 7:00pm)")
-myListbox.insert(5, "Brisbane to Singapore (Departure 7:00pm)")
-myListbox.insert(6, "Brisbane to London (Departure 7:10pm)")
-myListbox.insert(7, "Brisbane to LAX (Departure 7:10pm)")
-myListbox.insert(8, "Brisbane to Rome (Departure 7:30pm)")
-myListbox.grid(row=0, column=0, rowspan=7)
-
+#Again i would love if i could fit this code into the menu code.
+# MENU CODE
+menu = Menu(root)
+root.config(menu=menu)
+root.option_add("*tearOff", False)
+subMenu1 = Menu(menu)
+subMenu2 = Menu(menu)
+subMenu3 = Menu(menu)
+menu.add_cascade(label="File", menu=subMenu1)
+subMenu1.add_command(label="Exit?", command=root.destroy)
+menu.add_cascade(label="Images.exe", menu=subMenu2)
+subMenu2.add_command(label="HIDE THE PAIN HAROLD", command=hideThePainHarold)
+menu.add_cascade(label="YEE", menu=subMenu3)
+subMenu3.add_command(label="Yeebusters", command=root.destroy)
 
 root.mainloop()
